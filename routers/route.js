9a198fac47fcaa4d50ee8f3a36c90c4f4f2f3b46
Arnaldo jose Tomo <arnaldotomo@gmail.com>
2023-03-24 08:25:27 +0200

    Create route.js

---
+++ routers/route.js
@@ -0,0 +1,63 @@
+import * as React from 'react';
+import { Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import Index from '../src';
+
+function HomeScreen() {
+    return (
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+            <Text>Home!</Text>
+        </View>
+    );
+}
+
+function SettingsScreen() {
+    return (
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+            <Text>Settings!</Text>
+        </View>
+    );
+}
+
+const Tab = createBottomTabNavigator();
+
+export default function MyStack() {
+    return (
+        <NavigationContainer>
+            <Tab.Navigator
+                screenOptions={({ route }) => ({
+                    tabBarIcon: ({ focused, color, size }) => {
+                        let iconName;
+
+                        if (route.name === 'Home') {
+                            iconName = focused ? 'ios-home-outline' : 'ios-home-outline';
+                        } else if (route.name === 'Settings') {
+                            iconName = focused ? 'ios-person' : 'ios-person-outline';
+                        } else if (route.name === 'pesquisa') {
+                            iconName = focused ? 'ios-search' : 'ios-search-outline';
+                        } else if (route.name === 'cart') {
+                            iconName = focused ? 'ios-cart' : 'ios-cart-outline';
+
+                        }
+
+                        // You can return any component that you like here!
+                        return <Ionicons name={iconName} size={size} color={color} />;
+                    },
+                    tabBarActiveTintColor: 'blue',
+                    tabBarInactiveTintColor: 'blue',
+                })}
+            >
+                <Tab.Screen name="Home " component={Index} options={{
+                    headerShadowVisible: false, headerTitleStyle: {
+                    },
+
+                }} />
+                <Tab.Screen name="pesquisa" component={SettingsScreen} />
+                <Tab.Screen name="cart" component={SettingsScreen} />
+                <Tab.Screen name="Settings" component={SettingsScreen} />
+            </Tab.Navigator>
+        </NavigationContainer>
+    );
+}
